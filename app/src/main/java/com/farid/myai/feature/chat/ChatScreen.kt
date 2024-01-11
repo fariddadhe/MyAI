4018d180b05e76e1ef088a6c8cc7686e05071ded
Farid Ramadan <farid.ramadan@saltanera.com>
2024-01-11 14:56:57 +0900

    new ui

--- app/src/main/java/com/farid/myai/feature/chat/ChatScreen.kt
+++ app/src/main/java/com/farid/myai/feature/chat/ChatScreen.kt
@@ -5,25 +5,34 @@ import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AddCircle
+import androidx.compose.material.icons.outlined.Send
 import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.Send
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -31,6 +40,8 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -41,7 +52,11 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.ImageLoader
@@ -49,6 +64,11 @@ import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import coil.request.SuccessResult
 import coil.size.Precision
+import com.farid.myai.ui.theme.DarkGreen
+import com.farid.myai.ui.theme.GreyBlue
+import com.farid.myai.ui.theme.LightBlue
+import com.farid.myai.ui.theme.PrimaryColor
+import com.farid.myai.ui.theme.WhiteTextColor
 import com.farid.myai.util.UriSaver
 import kotlinx.coroutines.launch
 
@@ -64,6 +84,7 @@ fun ChatRoute(
     val imageLoader = ImageLoader.Builder(LocalContext.current).build()
 
     Scaffold(
+        containerColor = PrimaryColor,
         bottomBar = {
             MessageInput(
                 onReasonClicked = { inputText, selectedItems ->
@@ -118,7 +139,8 @@ private fun ChatList(
 ){
     LazyColumn(
         reverseLayout = true,
-        state = listState
+        state = listState,
+        modifier = Modifier.padding(horizontal = 16.dp)
     ){
         items(chatMessage.reversed()) { message ->
             ChatBubbleItem(message)
@@ -134,15 +156,15 @@ private fun ChatBubbleItem(
             chatMessage.participant == Participant.ERROR
 
     val backgroundColor = when (chatMessage.participant) {
-        Participant.MODEL -> MaterialTheme.colorScheme.primaryContainer
-        Participant.USER -> MaterialTheme.colorScheme.tertiaryContainer
+        Participant.MODEL -> DarkGreen
+        Participant.USER -> LightBlue
         Participant.ERROR -> MaterialTheme.colorScheme.errorContainer
     }
 
     val bubbleShape = if (isModelMessage) {
-        RoundedCornerShape(4.dp, 20.dp, 20.dp, 20.dp)
+        RoundedCornerShape(40.dp, 50.dp, 50.dp, 0.dp)
     } else {
-        RoundedCornerShape(20.dp, 4.dp, 20.dp, 20.dp)
+        RoundedCornerShape(50.dp, 40.dp, 0.dp, 50.dp)
     }
 
     val horizontalAlignment = if (isModelMessage) {
@@ -157,11 +179,11 @@ private fun ChatBubbleItem(
             .padding(horizontal = 8.dp, vertical = 4.dp)
             .fillMaxWidth()
     ) {
-        Text(
-            text = chatMessage.participant.name,
-            style = MaterialTheme.typography.bodySmall,
-            modifier = Modifier.padding(bottom = 4.dp)
-        )
+//        Text(
+//            text = chatMessage.participant.name,
+//            style = MaterialTheme.typography.bodySmall,
+//            modifier = Modifier.padding(bottom = 4.dp)
+//        )
         Row {
             if (chatMessage.isPending) {
                 CircularProgressIndicator(
@@ -177,6 +199,7 @@ private fun ChatBubbleItem(
                     modifier = Modifier.widthIn(0.dp, maxWidth * 0.9f)
                 ) {
                     Text(
+                        style = TextStyle(color = WhiteTextColor),
                         text = chatMessage.text,
                         modifier = Modifier.padding(16.dp)
                     )
@@ -190,10 +213,12 @@ private fun ChatBubbleItem(
                 items(chatMessage.selectedImages.size) { index ->
                     AsyncImage(
                         model = chatMessage.selectedImages[index],
+                        contentScale = ContentScale.Crop,
                         contentDescription = null,
                         modifier = Modifier
                             .padding(4.dp)
-                            .requiredSize(72.dp)
+                            .clip(RoundedCornerShape(8.dp))
+                            .size(80.dp)
                     )
                 }
             }
@@ -201,6 +226,7 @@ private fun ChatBubbleItem(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MessageInput(
     onReasonClicked: (String, List<Uri>?) -> Unit = { _, _ -> },
@@ -217,62 +243,76 @@ fun MessageInput(
         }
     }
 
-    Card(
-        modifier = Modifier.fillMaxWidth()
-    ) {
-        Row(
-            modifier = Modifier.padding(top = 16.dp)
+    Column(Modifier.padding(vertical = 24.dp, horizontal = 16.dp)) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
         ) {
-            IconButton(
-                onClick = {
-                    pickMedia.launch(
-                        PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(
+                    onClick = {
+                        pickMedia.launch(
+                            PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                        )
+                    },
+                ) {
+                    Icon(
+                        Icons.Outlined.AddCircle,
+                        contentDescription = "",
+                        tint = Color.White,
+                        modifier = Modifier.size(28.dp)
                     )
-                },
-                modifier = Modifier
-                    .padding(all = 4.dp)
-                    .align(Alignment.CenterVertically)
-            ) {
-                Icon(
-                    Icons.Rounded.Add,
-                    contentDescription = "",
+                }
+                OutlinedTextField(
+                    value = userMessage,
+                    label = { "Message" },
+                    placeholder = { "Masukkan pertanyaanmu" },
+                    onValueChange = { userMessage = it },
+                    colors = TextFieldDefaults.textFieldColors(
+                        containerColor = Color.Transparent,
+                        focusedIndicatorColor = LightBlue,
+                        unfocusedIndicatorColor = DarkGreen
+                    ),
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(horizontal = 4.dp)
                 )
-            }
-            OutlinedTextField(
-                value = userMessage,
-                label = { "Message" },
-                placeholder = { "Masukkan pertanyaanmu" },
-                onValueChange = { userMessage = it },
-                modifier = Modifier
-                    .fillMaxWidth(0.8f)
-            )
-            TextButton(
-                onClick = {
-                    if (userMessage.isNotBlank()) {
-                        onReasonClicked(userMessage, if(imageUris.isNotEmpty()) imageUris.toList() else null)
-                        resetScroll()
-                        userMessage = ""
-                        imageUris.clear()
-                    }
-                },
-                modifier = Modifier
-                    .padding(all = 4.dp)
-                    .align(Alignment.CenterVertically)
-            ) {
-                Text("Go")
+                IconButton(
+                    onClick = {
+                        if (userMessage.isNotBlank()) {
+                            onReasonClicked(userMessage, if(imageUris.isNotEmpty()) imageUris.toList() else null)
+                            resetScroll()
+                            userMessage = ""
+                            imageUris.clear()
+                        }
+                    },
+                ) {
+                    Icon(
+                        Icons.Outlined.Send,
+                        contentDescription = "",
+                        tint = if(userMessage.isNotBlank()) Color.White else GreyBlue,
+                    )
+                }
             }
         }
-        LazyRow(
-            modifier = Modifier.padding(all = 8.dp)
-        ) {
-            items(imageUris.size) { index ->
-                AsyncImage(
-                    model = imageUris[index],
-                    contentDescription = null,
-                    modifier = Modifier
-                        .padding(4.dp)
-                        .requiredSize(72.dp)
-                )
+
+        if(imageUris.isNotEmpty()){
+            LazyRow(
+                modifier = Modifier.padding(all = 8.dp)
+            ) {
+                items(imageUris.size) { index ->
+                    AsyncImage(
+                        model = imageUris[index],
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(4.dp)
+                            .requiredSize(72.dp)
+                    )
+                }
             }
         }
     }
